a179e1beb51e3bb324faeb0d685d215c9e00b155
soonb <soonb@DESKTOP-LPFSIKK>
2017-03-24 11:22:17 +0900

    자동차 출력 수정본

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -29,29 +29,31 @@ public class CarTest {
 		System.out.println("최대속도:"+car.getMaxspeed()+"km");
 		System.out.println("가격:"+String.format("%,d",car.getPrice())+"원");
 		System.out.println();
+		//
 		Car car2 = new Car();
 		car2.setCompany("기아자동차");
 		car2.setModel("K7");
 		car2.setColor("흰색");
 		car2.setMaxspeed(246);
 		car2.setPrice(40000000);
-		System.out.println("제조사명:"+car.getCompany());
-		System.out.println("모델명:"+car.getModel());
-		System.out.println("색상:"+car.getColor());
-		System.out.println("최대속도:"+car.getMaxspeed()+"km");
-		System.out.println("가격:"+String.format("%,d",car.getPrice())+"원");
+		System.out.println("제조사명:"+car2.getCompany());
+		System.out.println("모델명:"+car2.getModel());
+		System.out.println("색상:"+car2.getColor());
+		System.out.println("최대속도:"+car2.getMaxspeed()+"km");
+		System.out.println("가격:"+String.format("%,d",car2.getPrice())+"원");
 		System.out.println();
+		//
 		Car car3 = new Car();
 		car3.setCompany("삼성자동차");
 		car3.setModel("SM7");
 		car3.setColor("회색");
 		car3.setMaxspeed(200);
 		car3.setPrice(38000000);
-		System.out.println("제조사명:"+car.getCompany());
-		System.out.println("모델명:"+car.getModel());
-		System.out.println("색상:"+car.getColor());
-		System.out.println("최대속도:"+car.getMaxspeed()+"km");
-		System.out.println("가격:"+String.format("%,d",car.getPrice())+"원");
+		System.out.println("제조사명:"+car3.getCompany());
+		System.out.println("모델명:"+car3.getModel());
+		System.out.println("색상:"+car3.getColor());
+		System.out.println("최대속도:"+car3.getMaxspeed()+"km");
+		System.out.println("가격:"+String.format("%,d",car3.getPrice())+"원");
 
 	}
 
